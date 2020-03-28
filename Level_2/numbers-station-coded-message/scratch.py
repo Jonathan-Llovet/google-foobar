a528e8fed18793d0c35e2228b6ea3fc14984add3
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-03-28 17:51:15 -0400

    Reconciled differences with actual submission

--- Level_2/numbers-station-coded-message/scratch.py
+++ Level_2/numbers-station-coded-message/scratch.py
@@ -1,26 +1,44 @@
-l = [4, 3, 10, 2, 8]
-t2 = 12
+import timeit
+
+def solution(l, t):
+    # Increment end index for each starting index
+        # Keep track of intermediate calculations
+            # calculated = dict({})
+        # What's the best way to keep track of intermediate calculations?
+    for start_index in range(len(l)):
+        # print "start_index: %d" % start_index
+        for end_index in range(start_index, len(l)):
+            # print "end_index: %d" % end_index
+            #refactor out nested loop if possible
+            a = sum_sublist(l, start_index, end_index)
+            # print "stats a: %d start_index: %d end_index: %d" % (a, start_index, end_index)
+            if a == t:
+                return [start_index, end_index]
+            if a > t:
+                break
+    # If sublist cannot be found
+    return [-1, -1]
 
 def sum_sublist(l, start, end):
     if start == end:
         return l[start]
     subtotal = 0
-    for i in range(start, end):
+    for i in range(start, end+1):
         subtotal += l[i]
     return subtotal
 
+import random
+def make_list():
+    l = []
+    for i in range(random.randint(0,100)):
+        l.append(random.randint(0,100))
+    return l
 
-t = 12
-def solution(l, t):
-    for i in range(len(l)):
-        for j in range(i+1, len(l) + 1):
-            a = sum_sublist(l, i, j)
-            print "stats a: %d i: %d j: %d" % (a, i, j)
-            if a == t:
-                print "Found sublist!"
-                return [i,j]
-            if a > t:
-                print "Breaking. Subtotal larger than target %d" % t
-                break
+run_times = []
 
-print solution(l, t)
+for i in range(100000):
+    run_times.append(timeit.timeit("solution(" + str(make_list()) + ", " + str(random.randint(0, 250)) + ")", setup="from __main__ import solution, sum_sublist", number=10))
+
+average = sum(run_times)/(10*len(run_times))
+print "times run: {}".format(len(run_times))
+print "average {}".format(average)
--- Level_2/numbers-station-coded-message/solution.py
+++ Level_2/numbers-station-coded-message/solution.py
@@ -1,22 +1,17 @@
-import timeit
-
 def solution(l, t):
     # Increment end index for each starting index
         # Keep track of intermediate calculations
             # calculated = dict({})
         # What's the best way to keep track of intermediate calculations?
     for start_index in range(len(l)):
-        # print "start_index: %d" % start_index
+        # TODO: refactor out nested loop if possible
         for end_index in range(start_index, len(l)):
-            # print "end_index: %d" % end_index
-            #refactor out nested loop if possible
-            a = sum_sublist(l, start_index, end_index)
-            # print "stats a: %d start_index: %d end_index: %d" % (a, start_index, end_index)
-            if a == t:
+            subtotal = sum_sublist(l, start_index, end_index)
+            if subtotal == t:
                 return [start_index, end_index]
-            if a > t:
+            if subtotal > t:
                 break
-    # If sublist cannot be found
+    # Sublist was not found
     return [-1, -1]
 
 def sum_sublist(l, start, end):
@@ -26,19 +21,3 @@ def sum_sublist(l, start, end):
     for i in range(start, end+1):
         subtotal += l[i]
     return subtotal
-
-import random
-def make_list():
-    l = []
-    for i in range(random.randint(0,100)):
-        l.append(random.randint(0,100))
-    return l
-
-run_times = []
-
-for i in range(100000):
-    run_times.append(timeit.timeit("solution(" + str(make_list()) + ", " + str(random.randint(0, 250)) + ")", setup="from __main__ import solution, sum_sublist", number=10))
-
-average = sum(run_times)/(10*len(run_times))
-print "times run: {}".format(len(run_times))
-print "average {}".format(average)
