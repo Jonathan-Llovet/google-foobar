637674b94bef2984b6ca76b88a18d7635bb3b067
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-03-29 13:19:58 -0400

    Add find-the-access-codes challenge

---
+++ Level_3/find-the-access-codes/solution.py
@@ -0,0 +1,16 @@
+def solution(l):
+    """Returns the number of access codes that are present in the list l
+
+    The access codes contained in the list l are 'lucky triples'.
+    'Lucky triples' are defined as a tuple (x, y, z) where x divides y and y
+    divides z, and where the indices (xi, yj, zk) satisfy i < j < k, such as
+    (1, 2, 4).
+
+    Args:
+        l: List of integers potentially containing access codes
+
+    Returns:
+        An integer indicating how many access codes are present in list l
+        If no access codes are found, 0 is returned.
+    """
+    pass
