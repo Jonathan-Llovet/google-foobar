8934cf012bee3518a7442ff4d9c1fc2f7c16d0ba
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-03-28 20:15:07 -0400

    Timed first round of alternative solutions

--- Level_2/numbers-station-coded-message/solution.py
+++ Level_2/numbers-station-coded-message/solution.py
@@ -1,8 +1,4 @@
 def solution(l, t):
-    # Increment end index for each starting index
-        # Keep track of intermediate calculations
-            # calculated = dict({})
-        # What's the best way to keep track of intermediate calculations?
     for start_index in range(len(l)):
         # TODO: refactor out nested loop if possible
         for end_index in range(start_index, len(l)):
@@ -17,7 +13,22 @@ def solution(l, t):
 def sum_sublist(l, start, end):
     if start == end:
         return l[start]
-    subtotal = 0
-    for i in range(start, end+1):
-        subtotal += l[i]
-    return subtotal
+    if start < end:
+        return l[start] + sum_sublist(l, (start + 1), end)
+
+# Solution 1: Imperative sum_sublist
+# times run: 100000
+# average 5.09376835823e-05
+
+# Solution 2: Functional sum_sublist
+# times run: 100000
+# average 4.87596058846e-05
+
+# Solution 3: Functional sum_sublist, cache intermediate calculations
+# times run: 100000
+# average 0.000562442457676
+
+# Solution 4: Semi-functional sum_sublist defined outside solution,
+#             cache intermediate calculations
+# times run: 100000
+# average 0.00121669625998
