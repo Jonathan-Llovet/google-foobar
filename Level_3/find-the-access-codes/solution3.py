eb987520793ee64fd9bac8a84f0da0ad104ce844
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-03-31 00:34:37 -0400

    Debugged to pass local tests

--- Level_3/find-the-access-codes/solution3.py
+++ Level_3/find-the-access-codes/solution3.py
@@ -4,8 +4,8 @@ import timeit
 def solution(l):
     """Returns the number of access codes that are present in the list l
 
-    The access codes contained in the list l are 'lucky triples'.
-    'Lucky triples' are defined as a tuple (x, y, z) where x divides y and y
+    The access codes contained in the list l are "lucky triples".
+    "Lucky triples" are defined as a tuple (x, y, z) where x divides y and y
     divides z, and where the indices (xi, yj, zk) satisfy i < j < k, such as
     (1, 2, 4).
 
@@ -17,11 +17,14 @@ def solution(l):
         If no access codes are found, 0 is returned.
     """
 
+    if len(l) < 3:
+        return str(0)
+
     meta = generate_metadata(l)
-    print "final: {}".format(meta)
+    # print "final: {}".format(meta)
     candidates = find_access_code_candidates(meta)
     access_codes = remove_bad_code_candidates(meta, candidates)
-    print access_codes
+    # print access_codes
     return str(len(access_codes))
 
 def get_indices(l):
@@ -68,10 +71,10 @@ def calculate_divisors(meta):
                     # print "Appending candidate: {}".format(candidate)
                     k_divisors.append(candidate)
                     # print "k_divisors for {}: {}".format(unique_value, k_divisors)
-        print "k_divisors for {}: {}".format(unique_value, k_divisors)
+        # print "k_divisors for {}: {}".format(unique_value, k_divisors)
         meta.update({
                 unique_value: {
-                    "indices": k_indices,
+                    "indices": sorted(k_indices, reverse=True),
                     "divisors": k_divisors
                 }
             })
@@ -88,65 +91,134 @@ def generate_metadata(l):
 
 def find_access_code_candidates(meta):
     # solution([31, 31, 1, 4, 2, 62])
-    # final: {'1': {'indices': [2], 'divisors': [1]}, '62': {'indices': [5], 'divisors': [1, 62, 2, 31]}, '2': {'indices': [4], 'divisors': [1, 2]}, '4': {'indices': [3], 'divisors': [1, 2, 4]}, '31': {'indices': [0, 1], 'divisors': [1, 31]}}
+    # final: {"1": {"indices": [2], "divisors": [1]}, "62": {"indices": [5], "divisors": [1, 62, 2, 31]}, "2": {"indices": [4], "divisors": [1, 2]}, "4": {"indices": [3], "divisors": [1, 2, 4]}, "31": {"indices": [0, 1], "divisors": [1, 31]}}
 
     # solution([1, 1, 2])
-    # final: {'1': {'indices': [0, 1], 'divisors': [1]}, '2': {'indices': [2], 'divisors': [1, 2]}}
+    # final: {"1": {"indices": [0, 1], "divisors": [1]}, "2": {"indices": [2], "divisors": [1, 2]}}
     candidates = []
-    for i, k in meta.iteritems():
-        print str(i), str(k)
-        # 1 {'indices': [2], 'divisors': [1]}
-        # 62 {'indices': [5], 'divisors': [1, 62, 2, 31]}
-        # 2 {'indices': [4], 'divisors': [1, 2]}
-        # 4 {'indices': [3], 'divisors': [1, 2, 4]}
-        # 31 {'indices': [0, 1], 'divisors': [1, 31]}        
-        for divisor_1 in k['divisors']:
-            for divisor_2 in meta[str(divisor_1)]['divisors']:
-                if sorted((int(i), divisor_1, divisor_2)) not in candidates:
-                    candidates.append(sorted((int(i), divisor_1, divisor_2)))
+    for x, k in meta.iteritems():
+        # print str(x), str(k)
+        # 1 {"indices": [2], "divisors": [1]}
+        # 62 {"indices": [5], "divisors": [1, 62, 2, 31]}
+        # 2 {"indices": [4], "divisors": [1, 2]}
+        # 4 {"indices": [3], "divisors": [1, 2, 4]}
+        # 31 {"indices": [0, 1], "divisors": [1, 31]}        
+        for y in k["divisors"]:
+            for z in meta[str(y)]["divisors"]:
+                valid = validate_instance_count(meta, x, y, z)
+                if valid:
+                    if sorted((int(x), y, z)) not in candidates:
+                        candidates.append(sorted((int(x), y, z)))
     
-    print "candidates: {}".format(candidates)
+    # print "candidates: {}".format(candidates)
     return candidates
 
-def remove_bad_code_candidates(meta, candidates):
-    # for index in meta[str(divisor)]['indices']
+def validate_instance_count(meta, *args):
+    # print "in validate_instance_count"
+    args = list(map(int, args))
+    # print "args: {}".format(args)
+    for item in args:
+        # print "item: {}".format(item)
+        # print "args.count(int(item)): {}".format(args.count(int(item)))
+        # print 'meta[str(item)]["indices"]: {}'.format(meta[str(item)]["indices"])
+        if args.count(int(item)) > len(meta[str(item)]["indices"]):
+            return False
+    return True
+
+def remove_bad_code_candidates(meta, candidates):  
+    # print "in remove_bad_code_candidates"
+    # print "candidates: {}".format(candidates)
+    access_codes = list(candidates)
+    for candidate in candidates:
+        # print "candidate: {}".format(candidate)
+        # 1 {"indices": [2], "divisors": [1]}
+        # 62 {"indices": [5], "divisors": [1, 62, 2, 31]}
+        # 2 {"indices": [4], "divisors": [1, 2]}
+        # 4 {"indices": [3], "divisors": [1, 2, 4]}
+        # 31 {"indices": [0, 1], "divisors": [1, 31]}      
+        x, y, z = list(map(int, candidate))
+
+        # print "x: {}".format(x)
+        # print "y: {}".format(y)
+        # print "z: {}".format(z)
+
+        x_indices = meta[str(x)]["indices"]
+        y_indices = meta[str(y)]["indices"]
+        z_indices = meta[str(z)]["indices"]
+
+        # print "x_indices: {}".format(x_indices)
+        # print "y_indices: {}".format(y_indices)
+        # print "z_indices: {}".format(z_indices)
+
+        # yx_in_order = False
+        # zy_in_order = False
+
+        
+        # print "Checking indices..."
+        for xi in x_indices:
+            for yj in y_indices:
+                xy_in_order = False
+                yz_in_order = False
+                # print "xi: {} yj: {}".format(xi, yj)
+                if xi < yj:
+                    xy_in_order = True
+                if xy_in_order:
+                    for zk in z_indices:
+                        # print "yj: {} zk: {}".format(yj, zk)
+                        if yj < zk:
+                            # print "break 1: yj < zk"
+                            yz_in_order = True
+                            break
+                if (xy_in_order and yz_in_order):
+                    # print "break 2: xi < yj"
+                    break
+            if (xy_in_order and yz_in_order):
+                # print "break 3: for xi"
+                break
+        
+        if not (xy_in_order and yz_in_order):
+            # print "Removing candidate: {}".format(candidate)
+            access_codes.remove(candidate)
+
+
+
+    # for index in meta[str(divisor)]["indices"]
     # if 
     # Each number needs to have a divisor that in turn has a divisor in its list
     # Use divisor as a reference and then check whether there is an option in order
-
-    # Special cases for 1 and i
-    pass
+    return access_codes
 
 
 
 
 
-
-# def test(l, expected):
-#     print "solution({})".format(str(l))
-#     print "************************************************"
-#     result = solution(l)
-#     print "************************************************"
-#     print "expected: {}".format(expected)
-#     if result == expected:
-#         print "solution({})".format(str(l))
-#         print "PASSED"
-#     else:
-#         print "solution({})".format(str(l))
-#         print "FAILED"
-#     print "\n"
 
 def test(l, expected):
     print "solution({})".format(str(l))
     print "************************************************"
-    solution(l)
+    result = solution(l)
     print "************************************************"
+    print "expected: {}".format(expected)
+    print "result: {}".format(result)
+    if result == str(expected):
+        print "solution({})".format(str(l))
+        print "PASSED"
+    else:
+        print "solution({})".format(str(l))
+        print "FAILED"
+    print "\n"
 
-test([1, 1], 0)
-test([1, 1, 2], 1)
+# def test(l, expected):
+#     print "solution({})".format(str(l))
+#     print "************************************************"
+#     solution(l)
+#     print "************************************************"
+
+# test([1, 1], 0)
+# test([1, 1, 2], 1)
 # test([1, 1, 1, 1], 1)
-test([1, 1, 2, 1, 1], 2)
-test([1, 1, 2, 1, 1, 2], 3)
+# test([1, 1, 2, 1, 1], 2)
+# test([1, 1, 2, 1, 1, 2], 3)
 # test([1, 1, 2, 4, 1, 2], 5)
 # test([1, 1, 2, 1], 2)
 # test([1, 2, 3, 4, 5, 6], 3)
@@ -159,12 +231,12 @@ test([1, 1, 2, 1, 1, 2], 3)
 # test([6,5,3,2,1], 0)
 # test([], 0)
 # test([3,2,5,2,1,6,8],2)
-test([31, 31, 1, 4, 2, 62], 2)
+# test([31, 31, 1, 4, 2, 62], 2)
 
 # print "************************************************"
 
 
-"""
+
 def make_list(upper_length=2001, upper_int=1000000, fixed=None):
     l = []
     if fixed != None and isinstance(fixed, int):
@@ -197,4 +269,3 @@ for test in range(2):
     print "average list length {}".format(average_list_length)
 
     print "************************************************"
-"""
