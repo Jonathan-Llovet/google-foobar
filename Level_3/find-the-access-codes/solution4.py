79487400e190c557b515f30bf8f109c2adaf90f6
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-01 14:49:45 -0400

    Refactor to modularize finding outbound connections

--- Level_3/find-the-access-codes/solution4.py
+++ Level_3/find-the-access-codes/solution4.py
@@ -13,23 +13,34 @@ def solution(l):
         An integer indicating how many access codes are present in list l
         If no access codes are found, 0 is returned.
     """
-    graph = dict({})
+    if isinstance(l, list) and not l:
+        raise IndexError("l must be a non-empty list. Empty list received.")
 
     access_codes = 0
 
-    for i, v in enumerate(list(l[:-1])):
-        outbound_connections = []
-        for j, w in enumerate(l):
-            if j > i and w % v == 0:
-                outbound_connections.append(str(j))
-        if outbound_connections:
-            graph.update({str(i): outbound_connections})
+    # Transform list into an acyclic directed graph
+    graph = dict({})
+    for node_index, node_value in enumerate(list(l[:-1])):
+        outbounds = find_outbound_connections(l, node_index, node_value)
+        if outbounds:
+            graph.update({str(node_index): outbounds})
 
+    # find all paths with 2 steps
     for node in sorted(graph.keys()):
         connected_nodes = graph[node]
         for connected_node in connected_nodes:            
             access_codes += count_outbound_connections(graph, connected_node)
     return access_codes
+
+def find_outbound_connections(l, node_index, node_value):
+    outbound_connections = []
+    for destination_index, destination_value in enumerate(l):
+            # avoids processing items out of order
+            # outbound connections must be to later elements in list
+            if destination_index > node_index:
+                if destination_value % node_value == 0:
+                    outbound_connections.append(str(destination_index))
+    return outbound_connections
 
 def count_outbound_connections(graph, node):
     if node in graph:
